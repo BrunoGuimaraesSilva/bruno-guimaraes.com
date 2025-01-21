943b6bd6fe2bca2dd9b65754b776346fce252eda
Bruno Guimarães da Silva <bruno.sil16441@gmail.com>
2025-01-21 13:12:11 +0000

    feat: add new structure

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,17 @@
+import { HttpPostClient, HttpPostParams, HttpResponse, HttpStatusCode } from '../../../data/protocols/http';
+import axios from 'axios';
+
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+	async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+		let httpResponse;
+		try {
+			httpResponse = await axios.post(params.url, params.body);
+		} catch (error) {
+			httpResponse = {
+				status: HttpStatusCode.notFound,
+				body: 'Credenciais invalidas',
+			};
+		}
+		return httpResponse;
+	}
+}
