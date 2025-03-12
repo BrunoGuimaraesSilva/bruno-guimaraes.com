1fbab32d4c1e4f81d6d603aa0e5fd2c852e18441
Bruno Guimarães da Silva <bruno.sil16441@gmail.com>
2025-03-12 11:25:58 +0000

    fix: updated projects

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -8,6 +8,8 @@ import {
   HStack,
   Badge,
 } from "@chakra-ui/react";
+import { LIGHT, useColorMode } from "@presentation/ui/color-mode";
+import ResponsiveContainer from "@presentation/wrappers/ResponsiveContainer";
 import { t } from "i18next";
 import Link from "next/link";
 
@@ -22,37 +24,46 @@ interface MenuItem {
 const menuArray = t("projects:menu", { returnObjects: true }) as MenuItem[];
 
 export default function ProjectsPage() {
-  return (
-    <Box maxW="6xl" mx="auto" py={10} px={6}>
-      <Heading as="h1" size="xl" textAlign="center" mb={6}>
-        My Projects
-      </Heading>
 
-      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }}>
-        {menuArray.map((project, key) => (
-          <Link href={`/projects/${project.href}`}>
-            <Box key={key} p={4} borderRadius="lg" boxShadow="lg">
-              <Image
-                src={project.image}
-                alt={project.title}
-                borderRadius="md"
-                mb={4}
-              />
-              <VStack align="start">
-                <Heading as="h3" size="md">
-                  {project.title}
-                </Heading>
-                <Text fontSize="sm">{project.description}</Text>
-                <HStack>
-                  {project.technologies.map((tech) => (
-                    <Badge size="lg">{tech}</Badge>
-                  ))}
-                </HStack>
-              </VStack>
-            </Box>
-          </Link>
-        ))}
-      </SimpleGrid>
-    </Box>
+  const { colorMode } = useColorMode();
+
+
+  return (
+    <ResponsiveContainer>
+        <Heading as="h1" size="xl" textAlign="center" mb={6}>
+         {t('projects:title')}
+        </Heading>
+
+        <SimpleGrid columns={{ base: 1, md: 2 }}>
+          {menuArray.map((project, key) => (
+            <Link href={`/projects/${project.href}`}>
+              <Box key={key} p={4} borderRadius="lg" boxShadow="lg" 
+                _hover={{ 
+                  boxShadow: colorMode === LIGHT ? "2xl" : "0px 4px 6px rgba(255, 255, 255, 0.5)",
+
+                 }} 
+              >
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  borderRadius="md"
+                  mb={4}
+                />
+                <VStack align="start">
+                  <Heading as="h3" size="md">
+                    {project.title}
+                  </Heading>
+                  <Text fontSize="sm">{project.description}</Text>
+                  <HStack>
+                    {project.technologies.map((tech) => (
+                      <Badge size="lg">{tech}</Badge>
+                    ))}
+                  </HStack>
+                </VStack>
+              </Box>
+            </Link>
+          ))}
+        </SimpleGrid>
+    </ResponsiveContainer>
   );
 }
