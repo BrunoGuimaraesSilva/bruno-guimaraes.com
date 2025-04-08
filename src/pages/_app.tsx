3466872ce92c0ea1ee727c873541e5fc932bfc5c
Bruno Guimaraes <bruno.sil16441@gmail.com>
2025-04-08 15:57:59 +0100

    feat: add meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,17 +12,46 @@ import { usePageTitle } from "@hooks/usePageTitle";
 
 function App({ Component, pageProps }: AppProps) {
   const title = usePageTitle();
-  
+
   return (
     <>
       <Head>
         <title>{title} | Bruno Guimarães</title>
         <meta
           name="description"
-          content="Portfolio of Bruno Guimarães - Full-Stack Developer specializing in Next.js and TypeScript."
+          content="Portfolio of Bruno Guimarães – Full-Stack Developer specialized in PHP, TypeScript, and a passionate Go learner."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://bruno-guimaraes.com" />
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* Open Graph (WhatsApp, Facebook, LinkedIn) */}
+        <meta property="og:title" content={`$Bruno Guimarães`} />
+        <meta
+          property="og:description"
+          content="Full-Stack Developer with expertise in TypeScript, PHP and a growing love for Go. Explore my portfolio and projects."
+        />
+        <meta
+          property="og:image"
+          content="https://bruno-guimaraes.com/preview.png"
+        />
+        <meta property="og:url" content="https://bruno-guimaraes.com" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={`Bruno Guimarães`} />
+        <meta
+          name="twitter:description"
+          content="Explore the work of Bruno Guimarães – Full-Stack Developer focused on TypeScript, PHP, and learning Go."
+        /> */}
+        {/* <meta
+          name="twitter:image"
+          content="https://bruno-guimaraes.com/preview.png"
+        /> */}
       </Head>
+
       <ChakraProvider value={system}>
         <ThemeProvider
           enableColorScheme
